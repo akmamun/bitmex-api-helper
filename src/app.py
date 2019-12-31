6c5b2073dec7dfdeed1285ace9a53ae98f00ec73
Al Mamun Khan <almamunkhan04@gmail.com>
2020-01-01 00:34:54 +0600

    fixed routing slash error

--- src/app.py
+++ src/app.py
@@ -7,32 +7,39 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-client = bitmex.bitmex(api_key= <api_key>, api_secret = <api_secret>)  # Write api_key and api_secret in string
+# Write api_key and api_secret in string
+client = bitmex.bitmex(api_key="<bitmex_api_key>",
+                       api_secret="<bitmex_api_secret>")
+
 
 @app.route("/")
 def index():
     return "bitmex api"
 
 
-@app.route("data/order")
+@app.route("/data/order")
 def order():
-    order_data = client.Order.Order_getOrders(symbol="XBTUSD", reverse=True).result()  # order data
-    return json.dumps(order_data, default=str)  # default=str for datetime filter
+    order_data = client.Order.Order_getOrders(
+        symbol="XBTUSD", reverse=True).result()  # order data
+    # default=str for datetime filter
+    return json.dumps(order_data, default=str)
 
 
-@app.route("data/instrument")
+@app.route("/data/instrument")
 def instrument():
-    instrument_data = client.Instrument.Instrument_get(symbol="XBTUSD", reverse=True).result()  # Instrument data
+    instrument_data = client.Instrument.Instrument_get(
+        symbol="XBTUSD", reverse=True).result()  # Instrument data
     return json.dumps(instrument_data, default=str)
 
 
-@app.route("data/position")
+@app.route("/data/position")
 def position():
-    position_data = client.Position.Position_get(filter=json.dumps({'symbol': 'XBTUSD'})).result()
+    position_data = client.Position.Position_get(
+        filter=json.dumps({'symbol': 'XBTUSD'})).result()
     return json.dumps(position_data, default=str)
 
 
-@app.route("data/quote")
+@app.route("/data/quote")
 def quote():
     quote_data = client.Quote.Quote_get(symbol='XBTUSD').result()
     return json.dumps(quote_data, default=str)
